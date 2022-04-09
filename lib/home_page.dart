70409a933b3f2916684ad675bbbdca8429b99d8c
binary-park <wyparks2@gmail.com>
2022-04-09 23:17:05 +0900

    메시지를 추가하라

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -50,19 +50,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-// TODO: 12. 아래 메세지를 이용해서 FortuneCookie 인수를 만들고 _cookies에 추가해주세요.
-/*
-한줄 코드라도 서로 거들면 낫다.
-팀장 한마디에 천줄코드 안써도 된다.
-버그를 잡으려다 잡은버그 놓친다.
-아니쓴 코드에 오류날까?
-길고 짧은 것은 strlen을 써봐야 안다.
-보기 좋은 코드가 디버깅 하기 좋다.
-버그보고 놀란가슴 오타보고 놀란다.
-제 코드 구린줄 모른다.
-개 같이 코딩해서 정승처럼 사표쓴다.
-될성부른 코드는 들여쓰기부터 안다.
-*/
 // TODO: 13. FortuneCookieBox 클래스에 pick getter를 추가하여 FortuneCookie를 순서대로 반 반환해주세요.
 
 // TODO: 19. 이제 각자의 취향대로 앱을 꾸며주세요!
