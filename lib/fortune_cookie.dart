81e88a3d91fe166988174786d2b2328814365a82
binary-park <wyparks2@gmail.com>
2022-04-09 23:10:43 +0900

    FortuneCookie 클래스에 문자열 message와 상수 생성자를 추가하라

--- lib/fortune_cookie.dart
+++ lib/fortune_cookie.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 
-class FortuneCookie extends StatefulWidget {
-  const FortuneCookie({Key? key}) : super(key: key);
+class FortuneCookie {
+  final String message;
 
-  @override
-  State<FortuneCookie> createState() => _FortuneCookieState();
-}
-
-class _FortuneCookieState extends State<FortuneCookie> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  const FortuneCookie(this.message);
 }
